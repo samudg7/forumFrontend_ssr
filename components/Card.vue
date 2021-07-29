8fcf65c6df8666757c6148da781c9ac63a2ddfb5
Samuele <61940773+samudg7@users.noreply.github.com>
2021-07-29 18:02:28 +0200

    delete post

--- components/Card.vue
+++ components/Card.vue
@@ -7,7 +7,24 @@
         <h5 class="card-title">{{post.body}}</h5>
         <h5 class="card-title">{{post.name}}</h5>
         <h5 class="card-title">{{post.created_at}}</h5>
-
+       <button @click="destroy(post.id)" class="btn btn-outline-danger fa fa-trash fa-2x pull-right">
+<!-- <v-btn rounded color="#F93154" @click="destroy(post.id)"> -->
+                  <!-- Delete -->
+                  <!-- <v-icon color="orange">delete</v-icon> -->
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-trash-fill"
+                    viewBox="0 0 16 16"
+                  >
+                    <path
+                      d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"
+                    />
+                  </svg>
+       </button>
+                <!-- </v-btn> -->
       </div>
     </div>
   </div>
@@ -17,6 +34,18 @@
 	export default {
 		props: {
 			post: Object
-		}
+		},
+
+    methods: {
+
+
+    async destroy(id) {
+        await this.$axios.$delete(`/post/${id}`)
+        this.$router.push('/createPost')
+        this.$router.push('/posts')
+
+
+      }
+      }
 	}
 </script>
--- pages/createPost/index.vue
+++ pages/createPost/index.vue
@@ -6,17 +6,14 @@
       <div class="form-group">
         <label><strong>Titolo</strong></label>
         <input v-model="form.title" type="text" class="form-control" placeholder="Inserisci il titolo" autofocus>
-        <!-- <small class="form-text text-danger" v-if="errors.title">{{errors.title[0]}}</small> -->
       </div>
       <div class="form-group">
         <label><strong>Descrizione</strong></label>
         <textarea v-model="form.body" class="form-control" rows="5"></textarea>
-        <!-- <small class="form-text text-danger" v-if="errors.body">{{errors.body[0]}}</small> -->
       </div>
        <div class="form-group">
         <label><strong>Autore</strong></label>
         <textarea v-model="form.name" class="form-control" rows="5"></textarea>
-        <!-- <small class="form-text text-danger" v-if="errors.body">{{errors.body[0]}}</small> -->
       </div>
       <button type="submit" class="btn btn-primary" :disabled="disabled">Create</button>
     </form>
@@ -40,11 +37,6 @@ export default {
         await this.$axios.$post("/post", this.form)
         // .then(res => this.console.log(res.data))
         return this.$router.push('/')
-
-        // .then(res => this.$router.push(res.data))
-        // .then(res => this.$router.push("/posts"))
-
-        // .catch(error => console.log(error.response.data));
     }
   },
   //disabilita button se i campi sono vuoti
